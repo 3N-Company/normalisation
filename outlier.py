0d7fda91123ba2c26f13166a5690b76845595590
Alexander Chernykh <alexanderchernykh.ru@gmail.com>
2021-10-22 21:22:11 +0200

    Added iqr filter

---
+++ outlier.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from typing import Tuple
+import numpy as np
+
+class SmallUser:
+    def __init__(self, data):
+        self.user = data['user']["id"]
+        self.latitude = float(data['metadata']["position"]['latitude'])
+        self.longitude = float(data['metadata']["position"]['longitude'])
+        self.score = 0
+
+        # self.username = data['user']["username"]
+        # self.role = data['user']["role"]
+        #
+        #
+        # self.proposed_name = data['metadata']['name']
+        # self.photo_year = data['metadata']['photoYear']
+
+class Normalisation:
+    def __init__(self):
+        self.user_array = []
+        self.latitude_array = []
+        self.longitude_array = []
+        self.score_array = []
+
+    def iqr_lower_uperbound_calculation(self, frame:pd.DataFrame, column:str)-> Tuple[float,float]:
+        '''
+        Calculation of the
+        :param frame:
+        :return: upper and lower bounds
+        '''
+        # since we do the quantile over many columns we want only the temperature
+        # column having back -> therefore [0]
+        # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.quantile.html
+        # iqr = frame.quantile(0.75)[0] - frame.quantile(0.25)[0]
+        # iqr = frame[["latitude", "longitude"]].quantile(0.75) - frame[["latitude", "longitude"]].quantile(0.25)
+        # upper_bound = frame[["latitude", "longitude"]].quantile(0.75)[0] + 1.5 * iqr
+        # lower_bound = frame[["latitude", "longitude"]].quantile(0.25)[0] - 1.5 * iqr
+        iqr = frame[[column]].quantile(0.75)[0] - frame[[column]].quantile(0.25)[0]
+        upper_bound = frame[[column]].quantile(0.75)[0] + 1.5 * iqr
+        lower_bound = frame[[column]].quantile(0.25)[0] - 1.5 * iqr
+        return upper_bound, lower_bound
+
+    def zscore(self, value:float, mean_frame:float, stand_deviation_frame:float) -> float:
+        """
+        performs the zscore calculation
+        :param value: single temperature value
+        :return: calculated value -> if it is
+        """
+        z = (value - mean_frame) / stand_deviation_frame
+        if z < -3 or z > 3:
+            print('Z score Reset ' + str(z))
+            return np.NaN
+        return value
+
+    def provide_score_for_user(self):
+        """
+        # todo provide a score for a user
+        :return:
+        """
+        pass
+    @staticmethod
+    def lqr_help_func( x: float, upper_bound: float, lower_bound: float) -> float:
+        # performs the evaluation of the lqr
+        if x < lower_bound or x > upper_bound:
+            print('found NaN IQR')
+            x_new = np.NaN
+            return x_new
+        else:
+            return x
