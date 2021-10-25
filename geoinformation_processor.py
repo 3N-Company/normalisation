a379e77f7d96b2e5414e93865685f8e833058b6b
Alexander Chernykh <alexanderchernykh.ru@gmail.com>
2021-10-25 14:14:46 +0200

    added docu + flake8 and black stile

--- geoinformation_processor.py
+++ geoinformation_processor.py
@@ -1,20 +1,17 @@
-from outlier import SmallUser, Normalisation
-import http
-from flask_cors import CORS, cross_origin
-from flask import request, Flask, Response, jsonify, abort
-import geopy
+from outlier import ImageMetadata, Normalisation
+from flask import request, Flask
 import pandas as pd
 import json
-from typing import List, Dict
+from typing import List
 
 app = Flask(__name__)
 
-class Filter:
 
+class Filter:
     def __init__(self):
         self.normalis = Normalisation()
 
-    def prepare_df_to_filter(self, data:List[dict]) -> pd.DataFrame:
+    def prepare_df_to_filter(self, data: List[dict]) -> pd.DataFrame:
         """
         Preparing a pandas Dataframe for further
         Args:
@@ -24,17 +21,20 @@ class Filter:
             filled pandas dataframe
         """
         for elem in data:
-            user_data = SmallUser(elem)
+            user_data = ImageMetadata(elem)
             self.normalis.user_array.append(user_data.id)
             self.normalis.latitude_array.append(user_data.latitude)
             self.normalis.longitude_array.append(user_data.longitude)
             self.normalis.score_array.append(user_data.score)
 
-        d = {'id': self.normalis.user_array, 'latitude': self.normalis.latitude_array, "longitude": self.normalis.longitude_array,
-             "score": self.normalis.score_array}
+        d = {
+            "id": self.normalis.user_array,
+            "latitude": self.normalis.latitude_array,
+            "longitude": self.normalis.longitude_array,
+            "score": self.normalis.score_array,
+        }
         df = pd.DataFrame(data=d)
         return df
-
 
     def remove_outliers(self, data: List[dict]) -> bytes:
         """
@@ -57,35 +57,45 @@ class Filter:
         df = self.normalis.process_zscore(df, "latitude")
 
         # IQR FILTER
-        upper_bound_lat, lower_bound_lat = self.normalis.iqr_lower_uperbound_calculation(df, "latitude")
-        upper_bound_long, lower_bound_long = self.normalis.iqr_lower_uperbound_calculation(df, "longitude")
-        df['latitude'] = df['latitude'].map(lambda x: self.normalis.lqr_help_func(x, upper_bound_lat, lower_bound_lat))
-        df['longitude'] = df['longitude'].map(lambda x: self.normalis.lqr_help_func(x, upper_bound_long, lower_bound_long))
+        (
+            upper_bound_lat,
+            lower_bound_lat,
+        ) = self.normalis.iqr_lower_uperbound_calculation(df, "latitude")
+        (
+            upper_bound_long,
+            lower_bound_long,
+        ) = self.normalis.iqr_lower_uperbound_calculation(df, "longitude")
+        df["latitude"] = df["latitude"].map(
+            lambda x: self.normalis.lqr_help_func(x, upper_bound_lat, lower_bound_lat)
+        )
+        df["longitude"] = df["longitude"].map(
+            lambda x: self.normalis.lqr_help_func(x, upper_bound_long, lower_bound_long)
+        )
 
-
-        df['score'] = ~(df.isnull().any(axis=1))
-        frame_to_send = df[['id', 'score']]
-        mean_values = df[(df['latitude'].notna()) & (df['longitude'].notna())][['latitude', 'longitude']]
+        df["score"] = ~(df.isnull().any(axis=1))
+        frame_to_send = df[["id", "score"]]
+        mean_values = df[(df["latitude"].notna()) & (df["longitude"].notna())][
+            ["latitude", "longitude"]
+        ]
 
         reply, mean = {}, {}
         reply["mean"] = mean
-        mean['latitude'] = str(mean_values[['latitude']].mean().tolist()[0])
-        mean['longitude'] = str(mean_values[['longitude']].mean().tolist()[0])
-        score_json = frame_to_send.to_json(orient='records', force_ascii=False)
-        reply['user'] = json.loads(score_json)
+        mean["latitude"] = str(mean_values[["latitude"]].mean().tolist()[0])
+        mean["longitude"] = str(mean_values[["longitude"]].mean().tolist()[0])
+        score_json = frame_to_send.to_json(orient="records", force_ascii=False)
+        reply["user"] = json.loads(score_json)
 
-        filtered_data_with_score = json.dumps(reply, ensure_ascii=False).encode('utf8')
+        filtered_data_with_score = json.dumps(reply, ensure_ascii=False).encode("utf8")
         return filtered_data_with_score
 
 
-@app.route('/normalise', methods=['POST'])
+@app.route("/normalise", methods=["POST"])
 def main():
     data = request.get_json()
     filter = Filter()
-    ret = filter.remove_outliers(data)
+    filtered_data = filter.remove_outliers(data)
+    return filtered_data
 
-    return ret
 
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=2000)
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=2000)
--- outlier.py
+++ outlier.py
@@ -3,22 +3,27 @@ from typing import Tuple
 import numpy as np
 
 
-class SmallUser:
-    def __init__(self, data):
-        self.id = data['user']["id"]
-        self.latitude = float(data['metadata']["position"]['latitude']) # test on empty string
-        self.longitude = float(data['metadata']["position"]['longitude']) # test on empty string
-        self.score = False
+class ImageMetadata:
+    """
+    This class represents the submitted data, that will be further normalised
+    """
 
-        # self.username = data['user']["username"]
-        # self.role = data['user']["role"]
-        #
-        #
-        # self.proposed_name = data['metadata']['name']
-        # self.photo_year = data['metadata']['photoYear']
+    def __init__(self, data):
+        self.id = data["user"]["id"]
+        self.latitude = float(
+            data["metadata"]["position"]["latitude"]
+        )  # test on empty string
+        self.longitude = float(
+            data["metadata"]["position"]["longitude"]
+        )  # test on empty string
+        self.score = False
 
 
 class Normalisation:
+    """
+    This class represents the calculation of the IQR method and z- score
+    """
+
     def __init__(self):
         self.user_array = []
         self.latitude_array = []
@@ -26,15 +31,16 @@ class Normalisation:
         self.score_array = []
 
     @staticmethod
-    def iqr_lower_uperbound_calculation(frame: pd.DataFrame, column: str) -> Tuple[float, float]:
-        '''
-        Calculation of the
+    def iqr_lower_uperbound_calculation(
+        frame: pd.DataFrame, column: str
+    ) -> Tuple[float, float]:
+        """
+            since we do the quantile over many columns we want only for a single column
+            column having back -> therefore [0]
+            https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.quantile.html
         :param frame:
         :return: upper and lower bounds
-        '''
-        # since we do the quantile over many columns we want only the temperature
-        # column having back -> therefore [0]
-        # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.quantile.html
+        """
         iqr = frame[[column]].quantile(0.75)[0] - frame[[column]].quantile(0.25)[0]
         upper_bound = frame[[column]].quantile(0.75)[0] + 1.5 * iqr
         lower_bound = frame[[column]].quantile(0.25)[0] - 1.5 * iqr
@@ -44,36 +50,38 @@ class Normalisation:
     def zscore(value: float, mean_frame: float, stand_deviation_frame: float) -> float:
         """
         performs the zscore calculation
-        :param value: single temperature value
+        :param value: single value
         :return: calculated value -> if it is
         """
         z = (value - mean_frame) / stand_deviation_frame
         if z < -3 or z > 3:
-            print('Z score Reset ' + str(z))
+            print("Z score Reset " + str(z))
             return np.NaN
         return value
-
-    def provide_score_for_user(self):
-        """
-        # todo provide a score for a user
-        :return:
-        """
-        pass
 
     @staticmethod
     def lqr_help_func(x: float, upper_bound: float, lower_bound: float) -> float:
         # performs the evaluation of the lqr
         if x < lower_bound or x > upper_bound:
-            print('found NaN IQR')
+            print("found NaN IQR")
             x_new = np.NaN
             return x_new
         else:
             return x
 
+    def process_zscore(self, frame: pd.DataFrame, column_name: str) -> pd.DataFrame:
+        """
+        Calculate z-score basde on a column value
+        Args:
+            frame:
+            column_name:
 
-    def process_zscore(self, frame: pd.DataFrame, column_name: str):
-        # pandas function for standard deviation
+        Returns:
+
+        """
         stand_deviation_frame = frame[column_name].std(skipna=True)
         mean_frame = frame[column_name].mean()
-        frame[column_name] = frame[column_name].map(lambda x: self.zscore(x, mean_frame, stand_deviation_frame))
+        frame[column_name] = frame[column_name].map(
+            lambda x: self.zscore(x, mean_frame, stand_deviation_frame)
+        )
         return frame
